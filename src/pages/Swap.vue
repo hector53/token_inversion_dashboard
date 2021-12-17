41cc82ee9ff6f50655d45d50df3a84c856867f29
hector53 <hectoracosta5@gmail.com>
2021-12-17 13:36:40 -0400

    xxx

---
+++ src/pages/Swap.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <swapPrimary />
+  </div>
+</template>
+
+<script>
+import {defineComponent,defineAsyncComponent} from 'vue'
+export default defineComponent({
+     components: {
+          swapPrimary: defineAsyncComponent(() => import('components/swap/swapPrimary')),
+     },
+    setup () {
+        return {
+
+        }
+    }
+})
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -9,23 +9,7 @@ const routes = [
       }
       },
       {path: '/Swap', component: () => import('pages/Swap.vue')},
-      {path: '/Dashboard2', component: () => import('pages/Dashboard2.vue')},
-      {path: '/Profile', component: () => import('pages/UserProfile.vue')},
-      {path: '/Map', component: () => import('pages/Map.vue')},
-      {path: '/MapMarker', component: () => import('pages/MapMarker.vue')},
-      {path: '/TreeTable', component: () => import('pages/TreeTable.vue')},
-      {path: '/StreetView', component: () => import('pages/StreetView.vue')},
-      {path: '/Cards', component: () => import('pages/Cards.vue')},
-      {path: '/Tables', component: () => import('pages/Tables.vue')},
-      {path: '/Contact', component: () => import('pages/Contact.vue')},
-      {path: '/Checkout', component: () => import('pages/Checkout.vue')},
-      {path: '/Ecommerce', component: () => import('pages/ProductCatalogues.vue')},
-      {path: '/Pagination', component: () => import('pages/Pagination.vue')},
-      {path: '/Charts', component: () => import('pages/Charts.vue')},
-      {path: '/Calendar', component: () => import('pages/Calendar.vue')},
-      {path: '/Directory', component: () => import('pages/Directory.vue')},
-      {path: '/Footer', component: () => import('pages/Footer.vue')},
-      {path: '/CardHeader', component: () => import('pages/CardHeader.vue')},
+      
 
       // Not completed yet
       // {path: '/Taskboard', component: () => import('pages/TaskBoard.vue')},
@@ -34,26 +18,7 @@ const routes = [
 
   // Always leave this as last one,
   // but you can also remove it
-  {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/Error404.vue')
-  },
-  {
-    path: '/Mail',
-    component: () => import('layouts/Mail.vue')
-  },
-  {
-    path: '/Maintenance',
-    component: () => import('pages/Maintenance.vue')
-  },
-  {
-    path: '/Pricing',
-    component: () => import('pages/Pricing.vue')
-  },
-  {
-    path: '/Login-1',
-    component: () => import('pages/Login-1.vue')
-  },
+  
   {
     path: '/login',
     component: () => import('pages/login.vue')
@@ -62,14 +27,7 @@ const routes = [
     path: '/signup',
     component: () => import('pages/signup.vue')
   },
-  {
-    path: '/Lock',
-    component: () => import('pages/LockScreen.vue')
-  },
-  {
-    path: '/Lock-2',
-    component: () => import('pages/LockScreen-2.vue')
-  }
+  
 ]
 
 export default routes
